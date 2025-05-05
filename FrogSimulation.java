6c5db4d731a6fdf0a4f0ae0e2362b13114e7185c
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-05-05 23:52:01 +0000

    done

--- FrogSimulation.java
+++ FrogSimulation.java
@@ -1,19 +1,23 @@
+import java.util.Scanner;
+
 public class FrogSimulation
 {
 private int goalDistance;
 private int maxHops;
-private int[] presetHops;
 
 public FrogSimulation(int dist, int numHops)
 {
     goalDistance = dist;
     maxHops = numHops;
 }
+
 /** Returns an integer representing the distance, in inches, to be moved when the frog hops.
 */
 private int hopDistance()
 { 
-    
+    Scanner s = new Scanner(System.in);
+    System.out.println("Jump Distance: ");
+    return s.nextInt();
 }
 
 /** Simulates a frog attempting to reach the goal as described in part (a).
@@ -26,8 +30,8 @@ public boolean simulate()
     for(int i = 0; i < maxHops; i++)
     {
         pos += hopDistance();
-        if(position >= goalDistance) return true;
-        if(position < 0) return false;
+        if(pos >= goalDistance) return true;
+        if(pos < 0) return false;
     }
     return false;
 
@@ -38,5 +42,13 @@ public boolean simulate()
 * Precondition: num > 0
 */
 public double runSimulations(int num)
-{ /* to be implemented in part (b) */ }
+{ 
+    double success = 0;
+    for(int i = 0; i < num; i ++)
+    {
+        if(this.simulate()) success++;
+        System.out.println("next sim: ");
+    }
+    return success / num;
+ }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-
+        FrogSimulation sim = new FrogSimulation(24, 5);
+        System.out.println(sim.simulate());
+        System.out.println(sim.runSimulations(4));
     }
 }
